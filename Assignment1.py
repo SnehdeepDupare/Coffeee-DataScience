444c3d74d5c171ba3e688f8cc8c33c4564257b64
Snehdeep <duparesnehdeep@gmail.com>
2022-08-13 15:24:24 +0530

    Update Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,12 @@
+# Write a Python program which accepts a sequence of comma-separated numbers from the user and generates a list and a tuple with those numbers.
+
+# num = input("Enter some numbers: ")
+# lst = num.split(",")
+# tpl = tuple(lst)
+# print("List: ", lst)
+# print("Tuple: ", tpl)
+
+# Write a Python program to display the first and last colours from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+print("First Color: ", color_list[0])
+print("Last Color: ", color_list[3])
